df195fbcee06489f6ee67376aec3114f8c2d0bc1
danggai <donggi9313@naver.com>
2021-09-23 13:13:48 +0900

    - Project moved.   - Tdd Example

---
+++ TddSample/app/src/main/java/com/example/tddexample/util/CalculationUtils.kt
@@ -0,0 +1,15 @@
+package com.example.tddexample.util
+
+object CalculationUtils {
+    fun plus(origin: Int, dest: Int): Int {
+        return origin + dest
+    }
+
+    fun minus(origin: Int, dest: Int): Int {
+        return origin - dest
+    }
+
+    fun multiple(origin: Int, dest: Int): Int {
+        return origin * dest
+    }
+}
